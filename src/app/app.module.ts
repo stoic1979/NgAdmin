4373e4d8383d6e6f899ef8c7096b40f26442c48d
Roohi Verma <roohivermapb@gmail.com>
2018-01-26 21:23:09 +0530

    auth module - bare bones

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,19 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MdButtonModule } from '@angular/material';
+import 'hammerjs';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+	BrowserAnimationsModule,
+	MdButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
