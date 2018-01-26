c4a6e894005ae9bd5cb8cb421bf97cc323abb154
Roohi Verma <roohivermapb@gmail.com>
2018-01-26 21:53:50 +0530

    dependency setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -11,7 +13,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule,
+    BrowserModule, 
+    BrowserAnimationsModule
     AppRoutingModule
   ],
   providers: [],
