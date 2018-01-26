03df53cc6a8c807d75522973268686df2a3c2830
Roohi Verma <roohivermapb@gmail.com>
2018-01-26 21:32:54 +0530

    code refactor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
-
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MdButtonModule } from '@angular/material';
-import 'hammerjs';
 
 
 @NgModule({
@@ -15,8 +12,7 @@ import 'hammerjs';
   ],
   imports: [
     BrowserModule,
-	BrowserAnimationsModule,
-	MdButtonModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
